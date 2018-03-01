0ec3fc5e9b996b4beb97c365b0371e0b14cb13c0
Hiroki11x <hiroki11x@gmail.com>
2018-03-01 17:40:17 +0900

    update to add update time

--- sandbox/osc_sender_sample.py
+++ sandbox/osc_sender_sample.py
@@ -1,5 +1,7 @@
 import argparse
 
+import time
+
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
@@ -19,10 +21,12 @@ print("ip:127.0.0.1, port:" + str(port_num) + ", address:/data")
 
 def main():
   print("type int:")
-  input_str = input()
+  input_str = int(input())
+  update_time = str(time.asctime().split(" ")[4])
   msg = osc_message_builder.OscMessageBuilder(address="/data")
-  msg.add_arg(int(input_str))
-  print(input_str)
+  msg.add_arg(input_str)
+  msg.add_arg(update_time)
+  print(update_time)
   msg = msg.build()
   client.send(msg)
 
