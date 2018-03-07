73c4b85b3d341681cb36ed22179defbb880df8fa
Hiroki11x <hiroki11x@gmail.com>
2018-03-07 11:18:04 +0900

    fix for gl version

--- oscReceiver/src/main.cpp
+++ oscReceiver/src/main.cpp
@@ -3,15 +3,15 @@
 
 //========================================================================
 int main( ){
-//    ofSetupOpenGL(1024,768,OF_WINDOW);            // <-------- setup the GL context
+    ofSetupOpenGL(1024,768,OF_WINDOW);            // <-------- setup the GL context
 
-    ofGLFWWindowSettings settings;
-    settings.glVersionMajor = 2;
-    settings.glVersionMinor = 1;
-//    settings.setGLVersion(2,1);
-    settings.width = 1024;
-    settings.height = 768;
-    ofCreateWindow(settings);
+//    ofGLFWWindowSettings settings;
+//    settings.glVersionMajor = 2;
+//    settings.glVersionMinor = 1;
+////    settings.setGLVersion(2,1);
+//    settings.width = 1024;
+//    settings.height = 768;
+//    ofCreateWindow(settings);
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
