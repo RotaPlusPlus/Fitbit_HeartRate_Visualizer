76912358c1380ea52699d6f05b50028c313dba7f
Hiroki11x <hiroki11x@gmail.com>
2018-03-10 18:38:09 +0900

    add hongo

---
+++ oscReceiver/OSCManager.cpp
@@ -0,0 +1,8 @@
+//
+//  OSCManager.cpp
+//  oF_OSC_ReceiverSample
+//
+//  Created by HirokiNaganuma on 2018/03/10.
+//
+
+#include "OSCManager.hpp"
---
+++ oscReceiver/OSCManager.hpp
@@ -0,0 +1,13 @@
+//
+//  OSCManager.hpp
+//  oF_OSC_ReceiverSample
+//
+//  Created by HirokiNaganuma on 2018/03/10.
+//
+
+#ifndef OSCManager_hpp
+#define OSCManager_hpp
+
+#include <stdio.h>
+
+#endif /* OSCManager_hpp */
